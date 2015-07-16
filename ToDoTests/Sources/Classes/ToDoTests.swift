987d29c25d178490bfa2b16ce1c8c38a86cbe76c
Julian Grosshauser <julian@juliangrosshauser.com>
2015-07-16 21:34:13 +0200

    Add Xcode project

---
+++ ToDoTests/Sources/Classes/ToDoTests.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoTests.swift
+//  ToDoTests
+//
+//  Created by Julian Grosshauser on 16/07/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import XCTest
+@testable import ToDo
+
+class ToDoTests: XCTestCase {
+    
+}
