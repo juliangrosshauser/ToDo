fdf27acc5cae409481cb42158a072a2c08b8f315
Julian Grosshauser <julian@juliangrosshauser.com>
2015-07-17 23:08:43 +0200

    Add `GroupsCollectionViewController` class

---
+++ ToDo/Sources/Classes/View Controllers/GroupsCollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GroupCollectionViewController.swift
+//  ToDo
+//
+//  Created by Julian Grosshauser on 17/07/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+
+class GroupsCollectionViewController: UICollectionViewController {
+    
+}
