506fc0d0d0f28e425704e17097054e6e7ad711cb
Julian Grosshauser <julian@juliangrosshauser.com>
2015-07-21 23:15:29 +0200

    Add `ListsTableViewController` class

---
+++ ToDo/Sources/Classes/View Controllers/ListsTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ListsTableViewController.swift
+//  ToDo
+//
+//  Created by Julian Grosshauser on 21/07/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+
+class ListsTableViewController: UITableViewController {
+
+}
