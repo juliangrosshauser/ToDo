b02bd3297ece725f39167d651522bf11483f2d4e
Julian Grosshauser <julian@juliangrosshauser.com>
2015-07-28 16:57:43 +0200

    Add `ListViewModel` class

---
+++ ToDo/Sources/Classes/View Models/ListViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  ListViewModel.swift
+//  ToDo
+//
+//  Created by Julian Grosshauser on 28/07/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+class ListViewModel {
+    
+}
