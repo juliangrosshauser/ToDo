8eda04a5f48ebb9b541c109e121a2efe7527245c
Julian Grosshauser <julian@juliangrosshauser.com>
2015-07-29 17:34:25 +0200

    Add `ListControllerDelegate` protocol

---
+++ ToDo/Sources/Protocols/ListControllerDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  ListControllerDelegate.swift
+//  ToDo
+//
+//  Created by Julian Grosshauser on 29/07/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+protocol ListControllerDelegate: class {
+    
+    var list: List? { get set }
+}
