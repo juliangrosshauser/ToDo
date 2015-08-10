e9d916bf6130fe104ce97a84f76cc44f608bf9ad
Julian Grosshauser <julian@juliangrosshauser.com>
2015-08-11 00:06:02 +0200

    Add `Color` struct

---
+++ ToDo/Sources/Structs/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  ToDo
+//
+//  Created by Julian Grosshauser on 10/08/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+
+    static let lightBlue = UIColor(red:0.14, green:0.82, blue:0.99, alpha:1)
+}
