dcae1eebf04d1780484941c29fa38ec23ce70be6
Julian Grosshauser <julian@juliangrosshauser.com>
2015-08-12 23:43:45 +0200

    Add `RealmViewModel` class

---
+++ ToDo/Sources/Classes/View Models/RealmViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  RealmViewModel.swift
+//  ToDo
+//
+//  Created by Julian Grosshauser on 12/08/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+class RealmViewModel {
+
+}
