bddbb1899e93aae8e56245ab9f953d3ad89b5f39
Julian Grosshauser <julian@juliangrosshauser.com>
2015-08-16 00:11:38 +0200

    Add `TableViewCell` class

---
+++ ToDo/Sources/Classes/Views/TableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.swift
+//  ToDo
+//
+//  Created by Julian Grosshauser on 15/08/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+}
