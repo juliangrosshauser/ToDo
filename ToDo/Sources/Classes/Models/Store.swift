0bd40f55058723a4ab903ff40e8d1e94ac22eea5
Julian Grosshauser <julian@juliangrosshauser.com>
2015-08-29 00:35:11 +0200

    Send events instead of notifications
    Return `SignalProducer` after adding list/todo instead of sending notifications.

--- ToDo/Sources/Classes/Models/Store.swift
+++ ToDo/Sources/Classes/Models/Store.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import RealmSwift
+import ReactiveCocoa
 
 class Store {
 
@@ -15,9 +16,7 @@ class Store {
 
     private let realm = try! Realm()
     private var realmNotificationToken: NotificationToken?
-    static let listAddedNotification = "ListAddedNotification"
     static let listDeletedNotification = "ListDeletedNotification"
-    static let todoAddedNotification = "TodoAddedNotification"
     static let todoDeletedNotification = "TodoDeletedNotification"
     static let userInfoListIDKey = "ListID"
     private let notificationCenter = NSNotificationCenter.defaultCenter()
@@ -30,41 +29,28 @@ class Store {
 
     //MARK: Add List
 
-    func addList(name: String) {
-        dispatch_async(dispatch_get_global_queue(QOS_CLASS_UTILITY, 0)) {
-            let realm = try! Realm()
+    func addList(name: String) -> SignalProducer<Void, NoError> {
+        let list = List()
+        list.name = name
 
-            let list = List()
-            list.name = name
-
-            realm.write {
-                realm.add(list)
-            }
-
-            self.notificationCenter.postNotificationName(Store.listAddedNotification, object: self)
+        realm.write { [unowned self] in
+            self.realm.add(list)
         }
+
+        return SignalProducer(value: ())
     }
 
     //MARK: Append Todo To List
 
-    func appendTodo(text: String, list: List) {
-        let primaryKey = list.id
+    func appendTodo(text: String, list: List) -> SignalProducer<Void, NoError> {
+        let todo = Todo()
+        todo.text = text
 
-        dispatch_async(dispatch_get_global_queue(QOS_CLASS_UTILITY, 0)) {
-            let realm = try! Realm()
-
-            let todo = Todo()
-            todo.text = text
-
-            if let list = realm.objects(List).filter("id == %@", primaryKey).first {
-                realm.write {
-                    list.todos.append(todo)
-                }
-
-                let userInfo = [Store.userInfoListIDKey: list.id]
-                self.notificationCenter.postNotificationName(Store.todoAddedNotification, object: self, userInfo: userInfo)
-            }
+        realm.write {
+            list.todos.append(todo)
         }
+
+        return SignalProducer(value: ())
     }
 
     //MARK: Delete List
